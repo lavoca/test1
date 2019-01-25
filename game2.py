d0795afca9ad120522223571acbb2e14afde0101
lavoka <comoabdo@gmail.com>
2019-01-25 19:05:06 +0100

    Initial commit

---
+++ game2.py
@@ -0,0 +1,36 @@
+import random
+
+
+choice=['rock','scissors','paper']
+
+human_wins=[choice[0]+choice[1], choice[1]+choice[2], choice[2]+choice[0]]    
+    
+while True:
+    print('to quite press q')
+   
+    
+    human_choice=input('rock , scissors or paper: ')
+    computer_choice=random.choice(choice)
+    if human_choice == 'q':
+        break 
+    
+    
+    
+
+    if human_choice in choice:
+        print(computer_choice)
+        
+        if human_choice == 'q':
+           break
+        if human_choice==computer_choice:
+            print("it's a draw")
+
+        elif human_choice + computer_choice in human_wins :
+            print('human wins')
+
+        else:
+            print('computer wins')
+
+    else:
+        
+        print('try to type rock , scissors or paper in')
